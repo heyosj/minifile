f84dea83e65c3edabe6b3fbc767b1a64260cfe0a
osj <ojsanch@gmail.com>
2020-12-20 12:48:15 -0600

    test files added

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
-// TODO:
-// READ FILE
-// REMOVE WHITESPACE AND COMMENTS
-// WRITE NEW FILE WITH ALL THE SPACES AND COMMENTS REMOVED
+const fs = require('fs');
+
+// 1. READ FILE
+function readFile(filePath) {
+	fs.readFile(`${filePath}`, 'utf-8', (err, data) => {
+		if (err) {
+			console.log(err);
+			return;
+		}
+		console.log(data);
+	});
+}
+// 2. REMOVE WHITESPACE AND COMMENTS
+// 3. WRITE NEW FILE WITH ALL THE SPACES AND COMMENTS REMOVED
+
+readFile('./test-files/test.js');
